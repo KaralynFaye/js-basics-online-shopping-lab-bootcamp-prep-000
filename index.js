4f92eb503b57f73b076ff19bf9764e042603c52c
Karalyn Ferrari <lifesmygift@gmail.com>
2018-03-07 14:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
   }
 }
 
-//function placeOrder(cardNumber) {
+function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
